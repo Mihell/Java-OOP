959247f7f5e0ee344586592473e464f174415ad7
Mihell <ostapchukmv@gmail.com>
2018-07-19 20:45:41 +0300

    Update AccountRunner.java

--- Lesson6/src/ConnectionRetryAccount/AccountRunner.java
+++ Lesson6/src/ConnectionRetryAccount/AccountRunner.java
@@ -13,3 +13,20 @@ public class AccountRunner {
         }
     }
 }
+/*out:
+ConnectionRetryAccount.NotEnoughFundsException: Not enough funds!
+Witdraw of 20 dollars is success!
+	at ConnectionRetryAccount.RandomAccount.withdraw(RandomAccount.java:21)
+	at ConnectionRetryAccount.ConnectionRetryAccount.withdraw(ConnectionRetryAccount.java:15)
+	at ConnectionRetryAccount.AccountRunner.main(AccountRunner.java:9)
+ConnectionRetryAccount.AccountExpiredException: Your account is expired!
+	at ConnectionRetryAccount.RandomAccount.withdraw(RandomAccount.java:24)
+	at ConnectionRetryAccount.ConnectionRetryAccount.withdraw(ConnectionRetryAccount.java:15)
+	at ConnectionRetryAccount.AccountRunner.main(AccountRunner.java:9)
+ConnectionRetryAccount.AccountExpiredException: Your account is expired!
+	at ConnectionRetryAccount.RandomAccount.withdraw(RandomAccount.java:24)
+	at ConnectionRetryAccount.ConnectionRetryAccount.withdraw(ConnectionRetryAccount.java:15)
+	at ConnectionRetryAccount.AccountRunner.main(AccountRunner.java:9)
+
+Process finished with exit code 0
+*/
